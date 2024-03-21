c373463cb2db577c431dea369e670b1345eebe99
jiseok419 <pyosik0419@gmail.com>
2024-03-21 20:14:39 +0900

    Initial commit

---
+++ src/main/kotlin/com/dgsw/daechelinguide/DaechelinguideApplication.kt
@@ -0,0 +1,11 @@
+package com.dgsw.daechelinguide
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DaechelinguideApplication
+
+fun main(args: Array<String>) {
+    runApplication<DaechelinguideApplication>(*args)
+}
---
+++ src/test/kotlin/com/dgsw/daechelinguide/DaechelinguideApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dgsw.daechelinguide
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DaechelinguideApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
