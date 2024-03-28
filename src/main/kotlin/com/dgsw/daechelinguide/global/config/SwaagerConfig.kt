bddf13ba4323a56fbafdef292c12102eed31d536
jiseok419 <pyosik0419@gmail.com>
2024-03-28 16:49:01 +0900

    feat :: Swagger Config 추가

---
+++ src/main/kotlin/com/dgsw/daechelinguide/global/config/SwaagerConfig.kt
@@ -0,0 +1,22 @@
+package com.dgsw.daechelinguide.global.config
+
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+
+@Configuration
+class SwaggerConfig {
+    @Bean
+    fun openAPI(): OpenAPI {
+        val info: Info = Info()
+            .title("대슐랭 가이드")
+            .version("v1.0.0")
+            .description("대슐랭 가이드 API 문서")
+        return OpenAPI()
+            .components(Components())
+            .info(info)
+    }
+}
