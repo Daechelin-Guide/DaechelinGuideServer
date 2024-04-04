76ecae065e70fd67653b5b1aabf9bbd5a006f20d
jiseok419 <pyosik0419@gmail.com>
2024-04-04 20:44:38 +0900

    feat :: 급식 정보 불러오기 날짜 추가

--- src/main/kotlin/com/dgsw/daechelinguide/domain/menu/presentation/dto/MealResponse.kt
+++ src/main/kotlin/com/dgsw/daechelinguide/domain/menu/presentation/dto/MealResponse.kt
@@ -1,6 +1,7 @@
 package com.dgsw.daechelinguide.domain.menu.presentation.dto
 
 data class MealResponse(
+    val date: String,
     val breakfast: String?,
     val lunch: String?,
     val dinner: String?
