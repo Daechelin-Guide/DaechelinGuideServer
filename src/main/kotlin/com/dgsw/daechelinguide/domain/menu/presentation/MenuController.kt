399151085d4580a2c3755d36273c4c1724d33706
jiseok419 <pyosik0419@gmail.com>
2024-04-04 20:14:24 +0900

    feat :: Controller 추가

---
+++ src/main/kotlin/com/dgsw/daechelinguide/domain/menu/presentation/MenuController.kt
@@ -0,0 +1,28 @@
+package com.dgsw.daechelinguide.domain.menu.presentation
+
+import com.dgsw.daechelinguide.domain.menu.entity.MenuEntity
+import com.dgsw.daechelinguide.domain.menu.entity.value.MealType
+import com.dgsw.daechelinguide.domain.menu.presentation.dto.MealResponse
+import com.dgsw.daechelinguide.domain.menu.service.MenuService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/menu")
+class MenuController(
+    private val menuService: MenuService
+) {
+
+    @GetMapping
+    fun getMenu(@RequestParam(name = "date") date: String): MealResponse {
+        return menuService.getMeal(date)
+    }
+
+    @GetMapping("/detail")
+    fun getMenuDetail(@RequestParam(name = "date") date: String,
+                      @RequestParam(name = "mealType") mealType: MealType): MenuEntity {
+        return menuService.getMealDetail(date, mealType)
+    }
+}
