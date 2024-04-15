c19c4239f78dfcd634d580b10fa5166c814b6fb4
jiseok419 <pyosik0419@gmail.com>
2024-04-15 19:28:13 +0900

    feat :: menu 불러오기 query문 추가

--- src/main/kotlin/com/dgsw/daechelinguide/domain/menu/repository/MenuRepository.kt
+++ src/main/kotlin/com/dgsw/daechelinguide/domain/menu/repository/MenuRepository.kt
@@ -1,11 +1,13 @@
 package com.dgsw.daechelinguide.domain.menu.repository
 
-import com.dgsw.daechelinguide.domain.menu.entity.MenuEntity
+import com.dgsw.daechelinguide.domain.menu.entity.Menu
 import com.dgsw.daechelinguide.domain.menu.entity.value.MealType
 import org.springframework.data.repository.CrudRepository
 
-interface MenuRepository: CrudRepository<MenuEntity, Long> {
-    fun findAllByDate(date: String): List<MenuEntity>
+interface MenuRepository: CrudRepository<Menu, Long> {
+    fun findAllByDate(date: String): List<Menu>
 
-    fun findByDateAndMealType(date: String, mealType: MealType): MenuEntity?
+    fun findByDateAndMealType(date: String, mealType: MealType): Menu?
+
+    fun findMenuById(menuId: Long): Menu?
 }
