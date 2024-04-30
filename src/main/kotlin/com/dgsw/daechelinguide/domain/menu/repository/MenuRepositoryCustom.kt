e73b936d5d8424eff7a33c3fe6a089db10fccaa9
jiseok419 <pyosik0419@gmail.com>
2024-04-30 14:10:56 +0900

    feat :: Menu queryDSL 쿼리문 추가

--- src/main/kotlin/com/dgsw/daechelinguide/domain/menu/repository/MenuRepositoryCustom.kt
+++ src/main/kotlin/com/dgsw/daechelinguide/domain/menu/repository/MenuRepositoryCustom.kt
@@ -3,6 +3,7 @@ package com.dgsw.daechelinguide.domain.menu.repository
 import com.dgsw.daechelinguide.domain.menu.entity.Menu
 import com.dgsw.daechelinguide.domain.menu.entity.QMenu
 import com.dgsw.daechelinguide.domain.menu.entity.value.MealType
+import com.dgsw.daechelinguide.domain.vote.event.VoteMenuUploadEvent
 import com.querydsl.jpa.impl.JPAQueryFactory
 import org.springframework.stereotype.Repository
 
@@ -10,6 +11,14 @@ import org.springframework.stereotype.Repository
 class MenuRepositoryCustom(
     private val jpaQueryFactory: JPAQueryFactory
 ) {
+
+    fun findById(menuId: Long): Menu? {
+        val entity = QMenu.menu1
+
+        return jpaQueryFactory.selectFrom(entity)
+            .where(entity.id.eq(menuId))
+            .fetchOne()
+    }
 
     fun findAllMenuOrderByAsc(mealType: MealType): List<Menu> {
         val entity = QMenu.menu1
@@ -19,6 +28,16 @@ class MenuRepositoryCustom(
             .where(entity.mealType.eq(mealType))
             .orderBy(entity.totalScore.desc())
             .limit(10)
+            .fetch()
+    }
+
+    fun findAllDate(request: VoteMenuUploadEvent, mealType: MealType): List<Menu> {
+        val entity = QMenu.menu1
+
+        return jpaQueryFactory.selectFrom(entity)
+            .where(entity.date.stringValue().between(request.mealStartDate, request.mealEndDate))
+            .where(entity.mealType.eq(mealType))
+            .orderBy(entity.totalScore.desc())
             .fetch()
     }
 }
